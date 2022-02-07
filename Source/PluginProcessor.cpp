83f70f0e9cd315a74188cd0fd1db6038082f4b4c
MikeVSTs <mglglez@gmail.com>
2022-02-06 22:41:42 -0800

    Response Curve nos shows cut filter response

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -292,7 +292,7 @@ juce::AudioProcessorValueTreeState::ParameterLayout
 
     layout.add(std::make_unique<juce::AudioParameterFloat>("LowCut Freq", "LowCut Freq", juce::NormalisableRange<float>(20.f, 10000.f, 1.f, 0.25f), 20.f));
 
-    layout.add(std::make_unique<juce::AudioParameterFloat>("HighCut Freq", "HighCut Freq", juce::NormalisableRange<float>(5000.f, 20000.f, 1.f, 0.25f), 18000.f));
+    layout.add(std::make_unique<juce::AudioParameterFloat>("HighCut Freq", "HighCut Freq", juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f), 18000.f));
 
     layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Freq", "Peak Freq", juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f), 750.f));
 
