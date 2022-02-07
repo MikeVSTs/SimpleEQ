e62680020bf85ac2771fadedba9902a4c90a7718
MikeVSTs <mglglez@gmail.com>
2022-02-06 21:01:08 -0800

    Gave Editor its own MonoChain

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -30,7 +30,18 @@ struct ChainSettings
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState &apvts);
 
+// Type Aliases Creation to simplify the use of DSP on stereo processing
+using Filter = juce::dsp::IIR::Filter<float>;
+using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
 
+
+enum ChainPositions
+{
+    LowCut,
+    Peak,
+    HighCut
+};
 
 //==============================================================================
 /**
@@ -82,18 +93,7 @@ public:
     juce::AudioProcessorValueTreeState apvts{*this, nullptr, "Parameters", createParameterLayout()};
 
 private:
-    // Type Aliases Creation to simplify the use of DSP on stereo processing
-    using Filter = juce::dsp::IIR::Filter<float>;
-    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
-    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
     MonoChain leftChain, rightChain;
-
-    enum ChainPositions
-    {
-        LowCut,
-        Peak,
-        HighCut
-    };
 
     void updatePeakFilter(const ChainSettings &chainSettings);
 
